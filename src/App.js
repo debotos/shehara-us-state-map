62d95ef95908030a886a1cb9269ac6657435ae1d
Debotos Das <debotosdas@gmail.com>
2019-10-27 23:25:58 +0600

    init project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import MapGL, { Source, Layer } from '@urbica/react-map-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import keys from './config/keys'
+import data from './us-states.json'
+const { TOKEN, ZOOM } = keys
+
+class App extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			viewport: {
+				latitude: 41.850033,
+				longitude: -87.6500523,
+				zoom: ZOOM
+			}
+		}
+		this.onViewportChange = this.onViewportChange.bind(this)
+	}
+
+	onViewportChange(viewport) {
+		// close everything when user is zooming out
+		if (viewport.zoom < this.state.viewport.zoom) {
+			this.setState({ popupInfo: null })
+		}
+		this.setState({ viewport })
+	}
+
+	render() {
+		let { viewport } = this.state
+		console.log(data)
+		return (
+			<MapGL
+				style={{ width: '100vw', height: '100vh' }}
+				mapStyle='mapbox://styles/mapbox/light-v9'
+				accessToken={TOKEN}
+				onViewportChange={this.onViewportChange}
+				viewportChangeMethod='flyTo'
+				{...viewport}
+			>
+				<Source id='maine' type='geojson' data={data} />
+				<Layer
+					id='maine'
+					type='fill'
+					source='maine'
+					paint={{
+						'fill-color': '#088',
+						'fill-opacity': 0.8
+					}}
+				/>
+			</MapGL>
+		)
+	}
 }
 
-export default App;
+export default App
---
+++ src/config/keys.js
@@ -0,0 +1,5 @@
+export default {
+	TOKEN:
+		'pk.eyJ1IjoiZGVib3RvcyIsImEiOiJjanpmYXh0amYwYWhlM2pvNTdtamVtbXlqIn0.00E9hzDuT2nyeHF4LMlQAA',
+	ZOOM: 3 // 12 is fully zoomed
+}
